26874b7f0ae909f3d1993ec437c764da7fe0316c
Gene Liverman <gene.liverman+06301990@puppet.com>
2019-07-25 21:23:23 -0700

    (WIP) expose metrics via prometheus client

--- get-stats.py
+++ get-stats.py
@@ -1,5 +1,7 @@
 from datetime import datetime, time, timedelta
 import pprint
+from prometheus_client import start_http_server
+from prometheus_client.core import CounterMetricFamily, REGISTRY
 from pymongo import MongoClient
 import config
 client = MongoClient(config.SERVER)
@@ -9,7 +11,7 @@ categorizedFailures = []
 uncategorizedFailures = []
 midnight = datetime.combine(datetime.today(), time.min)
 yesterday_midnight = midnight - timedelta(days=1)
-dailyFailureCauses = failureCausesCollection.find({'lastOccurred': {'$gte': yesterday_midnight, '$lt': midnight}})
+dailyFailureCauses = failureCausesCollection.find({'lastOccurred': {'$gte': midnight}})
 for dailyFailureCause in dailyFailureCauses:
     if 'categories' in dailyFailureCause.keys():
         categorizedFailures.append(dailyFailureCause)
@@ -25,9 +27,9 @@ for categorizedFailure in categorizedFailures:
 
 failureCounts = []
 for uniqueCategory in uniqueCategories:
-    failures = failureCausesCollection.find({'lastOccurred': {'$gte': yesterday_midnight, '$lt': midnight}, 'categories': {'$in': [uniqueCategory]}})
+    failures = failureCausesCollection.find({'lastOccurred': {'$gte': midnight}, 'categories': {'$in': [uniqueCategory]}})
     categoryInfo = {}
-    categoryInfo['category'] = uniqueCategory
+    categoryInfo['category'] = uniqueCategory.lower()
     categoryInfo['failures'] = failures.count()
     failureCounts.append(categoryInfo)
 
@@ -36,4 +38,23 @@ uncategorizedFailureInfo['category'] = u'uncategorized'
 uncategorizedFailureInfo['failures'] = len(uncategorizedFailures)
 failureCounts.append(uncategorizedFailureInfo)
 
-pprint.pprint(failureCounts)
+# pprint.pprint(failureCounts)
+
+class CustomCollector(object):
+    def collect(self):
+        c = CounterMetricFamily('jenkins_bfa_failures_total', 'Total failures since midnight', labels=['failure_category'])
+        for failureCount in failureCounts:
+            metric_name = failureCount['category']
+            metric_value = failureCount['failures']
+            c.add_metric([metric_name], metric_value)
+        c.add_metric(['all'], dailyFailureCauses.count())
+        yield c
+
+REGISTRY.register(CustomCollector())
+
+
+if __name__ == '__main__':
+    # Start up the server to expose the metrics.
+    start_http_server(8000)
+    while True:
+        CustomCollector.collect
